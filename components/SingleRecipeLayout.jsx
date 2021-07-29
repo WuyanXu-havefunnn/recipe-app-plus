95d27c91bab63b9cc70b965343433a8765abc014
wuyanxubarhoumi <67730700+wuyanxubarhoumi@users.noreply.github.com>
2021-07-28 23:25:39 -0600

    Added max-width layout to Single Recipe

---
+++ components/SingleRecipeLayout.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+import Header from './Header'
+import Footer from './Footer'
+import { QUERIES } from '../lib/constants'
+
+export default function SingleRecipeLayout({ children }) {
+  return (
+    <Wrapper>
+      <Header />
+      <MaxWidthWrapper>
+        <Main>{children}</Main>
+      </MaxWidthWrapper>
+
+      <Footer />
+    </Wrapper>
+  )
+}
+
+const MaxWidthWrapper = styled.div`
+  max-width: 1024px;
+`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Main = styled.main`
+  flex: 1;
+  padding: 60px;
+  width: 100%;
+`
